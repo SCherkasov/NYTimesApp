e22f939763f4cb22305a5849f27a0d3dba430790
Stasik <scherkasov8@gmail.com>
2019-06-05 22:22:50 +0300

    trying to parse Json with Alamofire

--- NYTimesApp/Controller/FirstViewController.swift
+++ NYTimesApp/Controller/FirstViewController.swift
@@ -7,11 +7,58 @@
 //
 
 import UIKit
+import Alamofire
+
+struct All: Decodable {
+  let status: String?
+  let copyright: String?
+  let num_results: Int?
+  let results: [Results]
+}
+
+struct Results: Decodable {
+  let url: String?
+  let adx_keywords: String?
+  let column: Int?
+  let section: String?
+  let byline: String?
+  let type: String?
+  let title: String?
+  let abstract: String?
+  let published_date: String?
+  let source: String?
+  let id: Int?
+  let asset_id: Int?
+  let views: Int?
+  let des_facet: [String]
+  let org_facet: [String]
+  let per_facet: String
+  let geo_facet: [String]
+  let media: [Media]
+  let uri: String
+}
+
+struct Media: Decodable {
+  let type: String?
+  let subtype: String?
+  let caption: String?
+  let copyright: String?
+  let approved_for_syndication: Int?
+  let media_metadata: [Metadata]
+}
+
+struct Metadata: Decodable {
+  let url: String?
+  let format: String?
+  let height: Int?
+  let width: Int?
+}
 
 class FirstViewController: UIViewController {
   
   @IBOutlet var firstTableView: UITableView!
   
+ // var newArray = [All]()
   var array = ["1", "2", "3"]
 
     override func viewDidLoad() {
@@ -20,7 +67,49 @@ class FirstViewController: UIViewController {
       let nib = UINib(nibName: "FirstTableViewCell", bundle: nil)
       
       firstTableView.register(nib, forCellReuseIdentifier: "cell")
-  }
+
+      let urlString = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=MjXoOXYJ1lCIXHaEQZMZwg1yFCGKQzr7"
+      
+      AF.request(urlString).responseJSON { response in
+        //print(response)
+        
+        if let json = response.value {
+          let jsonDict = json as! Dictionary<String, Any>
+          let captinJs = jsonDict["results"] as! Array<Dictionary<String, Any>>
+          print(captinJs)
+          let abstrJs = captinJs["title"] as! Array<Any>
+          print(abstrJs)
+        }
+        
+      }
+print("Loading")
+//      guard let url = URL(string: urlString) else { return }
+//
+//      URLSession.shared.dataTask(with: url) { (data, response, error) in
+//        guard let data = data else { return }
+      
+        ///
+//        let dataString = String(data: data, encoding: .utf8)
+//        print(dataString)
+        ///
+//        do {
+//          guard let jsonJ = try JSONDecoder().decode(All.self, from: data) as? [String: Any] else { return }
+//          print(jsonJ)
+//        } catch let jsonErr{
+//          print("error serialization",jsonErr)
+//        }
+//        ///
+//      }.resume()
+//  }
+//        do {
+//          let json = try JSONDecoder().decode([All].self, from: data)
+//          print(json)
+//        } catch {
+//          print(error.localizedDescription)
+//        }
+//      }.resume()
+//}
+}
 }
 
 extension FirstViewController: UITableViewDataSource {
@@ -36,6 +125,5 @@ extension FirstViewController: UITableViewDataSource {
     
     return cell
   }
-  
   
 }
